b1dddb50b208dfa9ebc76ec106da352811eb2880
paulagjantunen-hue <paula.g.jantunen@gmail.com>
2026-09-12 17:45:37 +0000

    api to 3000

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.origin.replace("5173", "3001");
+const API_URL = window.location.origin.replace("5173", "3000");
 
 export async function getNotes() {
     const res = await fetch(`${API}/notes`);
